551b1e3ea663d253f8d8f5c0f68f7dc93bae629c
Pika <panic@thedis.co>
2018-09-22 14:43:59 -0400

    eee

---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+)
+
+const (
+	VERSION = "0.1"
+)
+
+type Release struct {
+	Assets []Asset `json:"assets"`
+}
+
+type Asset struct {
+	URL string `json:"browser_download_url"`
+}
+
+func printHelpAndExit() {
+	lines := []string{
+		fmt.Sprintf("nspbuild v%s by Pika", VERSION),
+		"usage: nspbuild <path/to/nso> <name> <author> <path/to/icon/jpg> <tid>",
+	}
+
+	for _, v := range lines {
+		fmt.Printf("%s\n", v)
+	}
+
+	os.Exit(0)
+}
+
+func getRelease(repo string) (Release, error) {
+	resp, err := http.Get(fmt.Sprintf("https://api.github.com/repos/%s/releases/latest", repo))
+	if err != nil {
+		return Release{}, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Release{}, err
+	}
+	resp.Body.Close()
+
+	r := Release{}
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		return Release{}, err
+	}
+
+	return r, nil
+}
+
+func main() {
+	os.Args = del(os.Args, 0)
+	if isEverythingNil(os.Args) {
+		printHelpAndExit()
+	}
+
+	args := org(parse(os.Args), []string{
+		"nso",
+		"name",
+		"author",
+		"icon",
+		"tid",
+	})
+
+	if !fileExists(args["nso"]) {
+		fmt.Printf("error: the file at %s does not exist!\n", args["nso"])
+		os.Exit(1)
+	} /* else if !fileExists(args["icon"]) {
+		fmt.Printf("error: the file at %s does not exist!", args["icon"])
+		os.Exit(1)
+	} */
+
+	if !isHex(args["tid"]) {
+		fmt.Printf("error: the title id %s is not valid hex!\n", args["tid"])
+		os.Exit(1)
+	} else if len(args["tid"]) != 16 {
+		fmt.Printf("error: the title id %s is not 16 characters!\n", args["tid"])
+	}
+
+	err := os.MkdirAll("build/", 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	linkle, err := getRelease("MegatonHammer/linkle")
+	if err != nil {
+		panic(err)
+	}
+
+	s := ""
+	if runtime.GOOS == "windows" && runtime.GOARCH == "386" {
+		// TODO: verify this arch
+		s = "i686-pc-windows-msvc.zip"
+	} else if runtime.GOOS == "windows" && runtime.GOARCH == "amd64" {
+		s = "x86_64-pc-windows-msvc.zip"
+	}
+
+	if s == "" {
+		fmt.Printf("error: linkle does not support this os/arch as of now, exiting...\n")
+		os.Exit(1)
+	}
+
+	url := ""
+	for _, v := range linkle.Assets {
+		if strings.HasSuffix(v.URL, s) {
+			url = v.URL
+		}
+	}
+
+	if url == "" {
+		fmt.Printf("error: could not find a linke build for your os/arch, exiting...\n")
+		os.Exit(1)
+	}
+
+	err = download(url, "build/linkle.zip")
+	if err != nil {
+		panic(err)
+	}
+
+	err = unzip("build/linkle.zip", "linkle.exe", "build/linkle.exe")
+	if err != nil {
+		panic(err)
+	}
+
+	hbp, err := getRelease("The-4n/hacBrewPack")
+	if err != nil {
+		panic(err)
+	}
+
+	err = download(hbp.Assets[0].URL, "build/hbp.zip")
+	if err != nil {
+		panic(err)
+	}
+
+	err = unzip("build/hbp.zip", "hacbrewpack.exe", "build/hbp.exe")
+	if err != nil {
+		panic(err)
+	}
+
+	if isAnythingNil(mapToSlice(args)) {
+		printHelpAndExit()
+	}
+}
---
+++ utils.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func isEverythingNil(list []string) bool {
+	nils := 0
+	for _, v := range list {
+		if v == "" {
+			nils += 1
+		}
+	}
+
+	if len(list) == nils {
+		return true
+	}
+
+	return false
+}
+
+func isAnythingNil(list []string) bool {
+	for _, v := range list {
+		if v == "" {
+			return true
+		}
+	}
+
+	return false
+}
+
+func del(list []string, index int) []string {
+	n := []string{}
+	for i, v := range list {
+		if i == index {
+			continue
+		}
+		n = append(n, v)
+	}
+
+	return n
+}
+
+func parse(list []string) []string {
+	pq := false
+	args := []string{}
+	q := []string{}
+	for _, v := range list {
+		if pq == false && !strings.HasPrefix(v, "\"") {
+			args = append(args, v)
+		} else if pq == false && strings.HasPrefix(v, "\"") {
+			pq = true
+			q = append(q, strings.TrimPrefix(v, "\""))
+		} else if pq == true && !strings.HasSuffix(v, "\"") {
+			q = append(q, v)
+		} else if pq == true && strings.HasSuffix(v, "\"") {
+			q = append(q, strings.TrimSuffix(v, "\""))
+			args = append(args, strings.Join(q, " "))
+			q = []string{}
+			pq = false
+		}
+	}
+
+	return args
+}
+
+func org(args []string, items []string) map[string]string {
+	m := map[string]string{}
+	for i, v := range args {
+		m[items[i]] = v
+	}
+
+	return m
+}
+
+func mapToSlice(m map[string]string) []string {
+	a := []string{}
+	for _, v := range m {
+		a = append(a, v)
+	}
+
+	return a
+}
+
+func fileExists(f string) bool {
+	_, err := os.Stat(f)
+	if err == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isHex(s string) bool {
+	r := regexp.MustCompile("\\A\\b[0-9a-fA-F]+\\b")
+	return r.MatchString(s)
+}
+
+func copy(src, dst string) error {
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	dest, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	_, err = io.Copy(dest, source)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func download(url, output string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	out, err := os.Create(output)
+	if err != nil {
+		return err
+	}
+
+	in := bytes.NewReader(body)
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+
+	out.Close()
+
+	return nil
+}
+
+func unzip(in, file, out string) error {
+	files, err := zip.OpenReader(in)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range files.File {
+		if v.Name == file {
+			source, err := v.Open()
+			if err != nil {
+				return err
+			}
+			defer source.Close()
+
+			dest, err := os.Create(out)
+			if err != nil {
+				return err
+			}
+			defer dest.Close()
+
+			_, err = io.Copy(dest, source)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
