66c5ca497bb43e4e1a6b94a97ea86c615e910e40
Pika <panic@thedis.co>
2018-10-20 17:31:25 -0400

    heck

--- main.go
+++ main.go
@@ -127,7 +127,7 @@ func main() {
 	scanner := bufio.NewScanner(resp.Body)
 
 	replacer := strings.NewReplacer("hbloader", args["name"], "0x010000000000100D",
-		"0x"+strings.ToLower(args["tid"]), "\"application_type\"   : 2", "\"application_type\"   : 0")
+		"0x"+strings.ToLower(args["tid"]), "\"application_type\"   : 2", "\"application_type\"   : 1")
 
 	for scanner.Scan() {
 		npdm.WriteString(replacer.Replace(scanner.Text()) + "\n")
